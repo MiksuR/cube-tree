0913ca38d39576477ffdb73eaddad45d116af86b
Miksu <miika.rankaviita@protonmail.com>
2022-10-08 19:09:52 +0100

    Write large scale structure of ConstructForest

--- src/ConstructForest.hs
+++ src/ConstructForest.hs
@@ -2,5 +2,23 @@ module ConstructForest where
 
 import Cube
 
-constructForest :: [CubeTree]
-constructForest = undefined
+import Data.Tree
+import Data.Set (Set, toList, delete)
+
+cubeForest :: Forest (Layer)
+cubeForest = unfoldForest buildNode firstCorners
+
+firstCorners :: [(Layer, Set Cubie, Set Cubie, Int)]
+firstCorners = [ (embedFirst (c, r), edges, delete c corners, 0)
+               | c <- toList corners, r <- [id, rotate, rotate . rotate] ]
+  where
+    embedFirst (c, r) = [CubieEmbed { cubie = r c,  location = (0, 0, 0) }]
+
+buildNode :: (Layer, Set Cubie, Set Cubie, Int) -> (Layer, [(Layer, Set Cubie, Set Cubie, Int)])
+buildNode (l, edgs, corns, n) = (l, filter (valid l) permutations)
+  where
+    permutations | mod n 2 == 0 = undefined
+                 | mod n 2 == 1 = undefined
+
+valid :: Layer -> (Layer, Set Cubie, Set Cubie, Int) -> Bool
+valid ol (nl, edgs, corns, n) = undefined
--- src/Cube.hs
+++ src/Cube.hs
@@ -1,6 +1,23 @@
 module Cube where
 
+import Data.Set
+
 data Color = W | Y | G | B | R | O deriving (Show, Eq, Enum, Ord)
-data Cubie = Edge Color Color | Corner Color Color Color deriving (Show)
-type Orient = Int
-type CubeTree = [[(Cubie, Orient)]]
+data Cubie = Edge !Color !Color |
+             Corner !Color !Color !Color deriving (Show, Eq, Ord)
+data CubieEmbed = CubieEmbed { cubie :: !Cubie,
+                               location :: !(Int, Int, Int) } deriving (Show)
+type Layer = [CubieEmbed]
+type Cube = [Layer]
+
+rotate :: Cubie -> Cubie
+rotate (Edge a b) = Edge b a
+rotate (Corner a b c) = Corner c a b
+
+corners :: Set Cubie
+corners = fromList [Corner W G O, Corner W O B, Corner W B R, Corner W R G,
+                    Corner Y O G, Corner Y B O, Corner Y R B, Corner Y G R]
+edges :: Set Cubie
+edges = fromList [Edge W G, Edge W O, Edge W B, Edge W R,
+                  Edge G O, Edge B O, Edge B R, Edge G R,
+                  Edge Y G, Edge Y O, Edge Y B, Edge Y R]
