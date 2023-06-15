1d8bdaed77179a46e10a33587878a108b98963e8
Miika <miika.rankaviita@protonmail.com>
2023-06-15 19:14:58 +0100

    Implement filtering and fix minor bugs

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
 module Main (main) where
 
+import ConstructForest
+import Cube
+
+import Data.Tree
+
+accumToLeaf :: Tree a -> [Int] -> [a]
+accumToLeaf _ [] = []
+accumToLeaf (Node x []) _ = [x]
+accumToLeaf t (i:is) = rootLabel subTree : accumToLeaf subTree is
+  where
+    subTree = subForest t !! i
+
 main :: IO ()
-main = undefined
+main = putStrLn . cubeNet . cubiesToCube
+                . (CubieEmbed {cubie = Corner W G O, location = (0,0,0)}:)
+                . concat $ accumToLeaf (head cubeForest) [0,0,0,0,0,0]
